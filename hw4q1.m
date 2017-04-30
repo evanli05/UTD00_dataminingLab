bdf5ad2ca359c3dca88165c6fd0a00f71baf54d4
Yifan Li <yifan.li.1112@gmail.com>
2017-04-30 13:27:42 -0500

    test
    test

---
+++ hw4q1.m
@@ -0,0 +1,9 @@
+function [d1, d2, d3] = hw4q1(x, y)
+
+d1 = sqrt((x-2)^2+(y-5)^2);
+d2 = sqrt((x-5)^2+(y-8)^2);
+d3 = sqrt((x-4)^2+(y-9)^2);
+
+end 
+
+
---
+++ hw4q2.m
@@ -0,0 +1,4 @@
+function se = hw4q2(x)
+
+se = (x-25)^2;
+end
